720aef62df7307d60fb7f050eb702da56bdf65e7
shobhit50 <shobhitparashar0@gmail.com>
2025-02-20 15:09:29 +0530

    Create initial file structure

---
+++ predefined_files/User.js
@@ -0,0 +1,13 @@
+const bcrypt = require('bcryptjs');
+
+
+const formatResponse = (data, message = 'Success') => {
+    return {
+        success: true,
+        message: message,
+        data: data,
+        timestamp: new Date().toISOString()
+    };
+};
+
+module.exports = {};
