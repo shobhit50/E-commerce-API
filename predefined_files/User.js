075a3dcda044252d3c7531750514b44144dc4b6d
shobhit50 <shobhitparashar0@gmail.com>
2025-02-26 19:10:29 +0530

    Add new feature implementation

---
+++ predefined_files/User.js
@@ -0,0 +1,13 @@
+const cors = require('cors');
+
+
+const formatResponse = (data, message = 'Success') => {
+    return {
+        success: true,
+        message: message,
+        data: data,
+        timestamp: new Date().toISOString()
+    };
+};
+
+module.exports = {};
