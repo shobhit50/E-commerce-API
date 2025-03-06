a0f324ac8be5481e417fca72e10a2ee660812755
shobhit50 <shobhitparashar0@gmail.com>
2025-03-06 11:40:30 +0530

    Add new component

---
+++ predefined_files/Payment.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+
+const validateInput = (data) => {
+    if (!data || typeof data !== 'object') {
+        throw new Error('Invalid input data');
+    }
+    return true;
+};
+
+module.exports = {};
