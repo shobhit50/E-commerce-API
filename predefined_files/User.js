f3129e33ad31d4389efe76604437cc4283cfc761
shobhit50 <shobhitparashar0@gmail.com>
2025-03-06 09:19:09 +0530

    Add new feature implementation

---
+++ predefined_files/User.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+
+const validateInput = (data) => {
+    if (!data || typeof data !== 'object') {
+        throw new Error('Invalid input data');
+    }
+    return true;
+};
+
+module.exports = {};
