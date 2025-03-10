f4f7c6b44a0c8ef0d97c46b28c431a5a985a497c
shobhit50 <shobhitparashar0@gmail.com>
2025-03-10 10:58:26 +0530

    Create utility functions

--- predefined_files/Cart.js
+++ predefined_files/Cart.js
@@ -1,20 +1,4 @@
-const mongoose = require('mongoose');
-
-
-const authenticateUser = async (req, res, next) => {
-    try {
-        const token = req.headers.authorization?.split(' ')[1];
-        if (!token) {
-            return res.status(401).json({ message: 'No token provided' });
-        }
-        // Token validation logic here
-        next();
-    } catch (error) {
-        res.status(401).json({ message: 'Invalid token' });
-    }
-};
-
-module.exports = {};
+const bcrypt = require('bcryptjs');
 
 
 const formatResponse = (data, message = 'Success') => {
@@ -25,3 +9,5 @@ const formatResponse = (data, message = 'Success') => {
         timestamp: new Date().toISOString()
     };
 };
+
+module.exports = {};
