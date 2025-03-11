6b0b4e9be0244781a655cb5d7dc04af70bda0c28
shobhit50 <shobhitparashar0@gmail.com>
2025-03-11 17:43:12 +0530

    Add new component

--- predefined_files/Order.js
+++ predefined_files/Order.js
@@ -1,19 +1,17 @@
 const express = require('express');
 
 
-const handleError = (error, req, res, next) => {
-    console.error('Error:', error.message);
-    res.status(500).json({
-        success: false,
+const authenticateUser = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization?.split(' ')[1];
+        if (!token) {
+            return res.status(401).json({ message: 'No token provided' });
+        }
+        // Token validation logic here
+        next();
+    } catch (error) {
+        res.status(401).json({ message: 'Invalid token' });
+    }
 };
 
 module.exports = {};
-
-
-
-const validateInput = (data) => {
-        throw new Error('Invalid input data');
-    }
-    return true;
-};
-// FIXME: Optimize this query
