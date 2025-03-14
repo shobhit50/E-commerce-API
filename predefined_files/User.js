e37c6afa92e271e17ce7ab563fb46cb53260dbdc
shobhit50 <shobhitparashar0@gmail.com>
2025-03-14 14:04:55 +0530

    Add new feature implementation

--- predefined_files/User.js
+++ predefined_files/User.js
@@ -1,30 +1,4 @@
-const mongoose = require('mongoose');
-
-
-const authenticateUser = async (req, res, next) => {
-    try {
-        const token = req.headers.authorization?.split(' ')[1];
-        if (!token) {
-            return res.status(401).json({ message: 'No token provided' });
-        }
-        // Token validation logic here
-        next();
-    } catch (error) {
-        res.status(401).json({ message: 'Invalid token' });
-    }
-};
-
-module.exports = {};
-
-
-const formatResponse = (data, message = 'Success') => {
-    return {
-        success: true,
-        message: message,
-        data: data,
-        timestamp: new Date().toISOString()
-    };
-};
+const express = require('express');
 
 
 const validateInput = (data) => {
@@ -33,3 +7,5 @@ const validateInput = (data) => {
     }
     return true;
 };
+
+module.exports = {};
