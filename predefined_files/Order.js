873301e20e4959b51a123506247457aef7aedc28
shobhit50 <shobhitparashar0@gmail.com>
2025-03-20 16:00:02 +0530

    Create initial file structure

--- predefined_files/Order.js
+++ predefined_files/Order.js
@@ -1,14 +1,4 @@
-const bcrypt = require('bcryptjs');
-
-
-const validateInput = (data) => {
-    if (!data || typeof data !== 'object') {
-        throw new Error('Invalid input data');
-    }
-    return true;
-};
-
-module.exports = {};
+const jwt = require('jsonwebtoken');
 
 
 const handleError = (error, req, res, next) => {
@@ -18,3 +8,5 @@ const handleError = (error, req, res, next) => {
         message: error.message
     });
 };
+
+module.exports = {};
