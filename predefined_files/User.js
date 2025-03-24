8e465626394b242e7981aa82bdb37ebec5e35172
shobhit50 <shobhitparashar0@gmail.com>
2025-03-24 16:39:36 +0530

    Add new feature implementation

--- predefined_files/User.js
+++ predefined_files/User.js
@@ -1,11 +1,12 @@
-const dotenv = require('dotenv');
+const express = require('express');
 
 
-const validateInput = (data) => {
-    if (!data || typeof data !== 'object') {
-        throw new Error('Invalid input data');
-    }
-    return true;
+const handleError = (error, req, res, next) => {
+    console.error('Error:', error.message);
+    res.status(500).json({
+        success: false,
+        message: error.message
+    });
 };
 
 module.exports = {};
