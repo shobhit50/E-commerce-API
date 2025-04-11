4c15b7724188a5c5c363df8695a7474487b7e73d
shobhit50 <shobhitparashar0@gmail.com>
2025-04-11 12:08:28 +0530

    Create initial file structure

--- predefined_files/Payment.js
+++ predefined_files/Payment.js
@@ -1,11 +1,13 @@
-const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 
-const validateInput = (data) => {
-    if (!data || typeof data !== 'object') {
-        throw new Error('Invalid input data');
-    }
-    return true;
+const formatResponse = (data, message = 'Success') => {
+    return {
+        success: true,
+        message: message,
+        data: data,
+        timestamp: new Date().toISOString()
+    };
 };
 
 module.exports = {};
