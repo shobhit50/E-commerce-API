c81c1b0a3971fdfaf70a6c82c63d717771c3219c
shobhit50 <shobhitparashar0@gmail.com>
2025-04-13 15:05:25 +0530

    Improve error handling

--- predefined_files/Payment.js
+++ predefined_files/Payment.js
@@ -11,3 +11,13 @@ const formatResponse = (data, message = 'Success') => {
 };
 
 module.exports = {};
+
+
+const formatResponse = (data, message = 'Success') => {
+    return {
+        success: true,
+        message: message,
+        data: data,
+        timestamp: new Date().toISOString()
+    };
+};
