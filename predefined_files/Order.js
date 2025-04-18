edd0a48e21e36970b21707515c7f0edf8c1247b8
shobhit50 <shobhitparashar0@gmail.com>
2025-04-18 18:51:24 +0530

    Refactor code structure

--- predefined_files/Order.js
+++ predefined_files/Order.js
@@ -10,3 +10,12 @@ const handleError = (error, req, res, next) => {
 };
 
 module.exports = {};
+
+
+const handleError = (error, req, res, next) => {
+    console.error('Error:', error.message);
+    res.status(500).json({
+        success: false,
+        message: error.message
+    });
+};
