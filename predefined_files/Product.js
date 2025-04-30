8956b316cc66ce3eadc549761724069eb8b64994
shobhit50 <shobhitparashar0@gmail.com>
2025-04-30 18:53:05 +0530

    Add new component

--- predefined_files/Product.js
+++ predefined_files/Product.js
@@ -1,23 +1,17 @@
-const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 
-const formatResponse = (data, message = 'Success') => {
-    return {
-        success: true,
-        message: message,
-        data: data,
-        timestamp: new Date().toISOString()
-    };
+const authenticateUser = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization?.split(' ')[1];
+        if (!token) {
+            return res.status(401).json({ message: 'No token provided' });
+        }
+        // Token validation logic here
+        next();
+    } catch (error) {
+        res.status(401).json({ message: 'Invalid token' });
+    }
 };
 
 module.exports = {};
-
-
-const handleError = (error, req, res, next) => {
-    console.error('Error:', error.message);
-    res.status(500).json({
-        success: false,
-        message: error.message
-    });
-};
-// FIXME: Optimize this query
