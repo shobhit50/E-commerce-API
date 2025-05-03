ffed0424c697d7299b1df2d9671b2ffe11731c37
shobhit50 <shobhitparashar0@gmail.com>
2025-05-03 13:41:41 +0530

    Refactor code structure

--- predefined_files/Product.js
+++ predefined_files/Product.js
@@ -10,3 +10,17 @@ const handleError = (error, req, res, next) => {
 };
 
 module.exports = {};
+
+
+const authenticateUser = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization?.split(' ')[1];
+        if (!token) {
+            return res.status(401).json({ message: 'No token provided' });
+        }
+        // Token validation logic here
+        next();
+    } catch (error) {
+        res.status(401).json({ message: 'Invalid token' });
+    }
+};
