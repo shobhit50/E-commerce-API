15bba2f1cd9ac46b63320caf0610424cbc900619
shobhit50 <shobhitparashar0@gmail.com>
2025-05-06 15:34:54 +0530

    Create utility functions

--- predefined_files/User.js
+++ predefined_files/User.js
@@ -1,17 +1,12 @@
-const express = require('express');
+const bcrypt = require('bcryptjs');
 
 
-const authenticateUser = async (req, res, next) => {
-    try {
-        const token = req.headers.authorization?.split(' ')[1];
-        if (!token) {
-            return res.status(401).json({ message: 'No token provided' });
-        }
-        // Token validation logic here
-        next();
-    } catch (error) {
-        res.status(401).json({ message: 'Invalid token' });
-    }
+const handleError = (error, req, res, next) => {
+    console.error('Error:', error.message);
+    res.status(500).json({
+        success: false,
+        message: error.message
+    });
 };
 
 module.exports = {};
