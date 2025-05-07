8e2dcea4f6990f6b9b676226bbc2b37dd4d86ecf
shobhit50 <shobhitparashar0@gmail.com>
2025-05-07 12:11:05 +0530

    Add new component

--- predefined_files/User.js
+++ predefined_files/User.js
@@ -1,12 +1,11 @@
-const bcrypt = require('bcryptjs');
+const dotenv = require('dotenv');
 
 
-const handleError = (error, req, res, next) => {
-    console.error('Error:', error.message);
-    res.status(500).json({
-        success: false,
-        message: error.message
-    });
+const validateInput = (data) => {
+    if (!data || typeof data !== 'object') {
+        throw new Error('Invalid input data');
+    }
+    return true;
 };
 
 module.exports = {};
