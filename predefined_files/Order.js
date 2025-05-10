7404b04cf44ced8f65158f64a97122c492c3edef
shobhit50 <shobhitparashar0@gmail.com>
2025-05-10 08:25:24 +0530

    Refactor code structure

--- predefined_files/Order.js
+++ predefined_files/Order.js
@@ -15,3 +15,12 @@ const authenticateUser = async (req, res, next) => {
 };
 
 module.exports = {};
+
+
+const handleError = (error, req, res, next) => {
+    console.error('Error:', error.message);
+    res.status(500).json({
+        success: false,
+        message: error.message
+    });
+};
