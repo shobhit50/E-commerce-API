93f39243ebab95e3f831c20c4fa018dcaf141da3
shobhit50 <shobhitparashar0@gmail.com>
2025-05-12 05:57:24 +0530

    Add new component

--- predefined_files/Order.js
+++ predefined_files/Order.js
@@ -1,4 +1,4 @@
-const dotenv = require('dotenv');
+const express = require('express');
 
 
 const authenticateUser = async (req, res, next) => {
@@ -15,26 +15,3 @@ const authenticateUser = async (req, res, next) => {
 };
 
 module.exports = {};
-
-
-const handleError = (error, req, res, next) => {
-    console.error('Error:', error.message);
-    res.status(500).json({
-        success: false,
-        message: error.message
-    });
-};
-
-
-const authenticateUser = async (req, res, next) => {
-    try {
-        const token = req.headers.authorization?.split(' ')[1];
-        if (!token) {
-            return res.status(401).json({ message: 'No token provided' });
-        }
-        // Token validation logic here
-        next();
-    } catch (error) {
-        res.status(401).json({ message: 'Invalid token' });
-    }
-};
