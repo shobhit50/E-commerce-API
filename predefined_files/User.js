dd7b997a07846c0efd11b2a6f94cd8fe1f28b32c
shobhit50 <shobhitparashar0@gmail.com>
2025-05-17 11:14:08 +0530

    Remove deprecated code

--- predefined_files/User.js
+++ predefined_files/User.js
@@ -5,8 +5,6 @@ const formatResponse = (data, message = 'Success') => {
     return {
         success: true,
         message: message,
-        data: data,
         timestamp: new Date().toISOString()
-    };
 };
 module.exports = {};
