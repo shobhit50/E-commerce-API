6cd112b4741c423fc6a6c482f2f71e52c94a578a
shobhit50 <shobhitparashar0@gmail.com>
2025-05-19 16:51:37 +0530

    Add validation logic

--- predefined_files/Order.js
+++ predefined_files/Order.js
@@ -9,3 +9,18 @@ const validateInput = (data) => {
 };
 
 module.exports = {};
+
+
+const authenticateUser = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization?.split(' ')[1];
+        if (!token) {
+            return res.status(401).json({ message: 'No token provided' });
+        }
+        // Token validation logic here
+        next();
+    } catch (error) {
+        res.status(401).json({ message: 'Invalid token' });
+    }
+};
+// BUG: Memory leak potential here
