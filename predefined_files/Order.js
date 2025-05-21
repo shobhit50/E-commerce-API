6caf2416a66b77c37fd40ea84fc5b1dfc62a4b20
shobhit50 <shobhitparashar0@gmail.com>
2025-05-21 09:00:03 +0530

    Remove deprecated code

--- predefined_files/Order.js
+++ predefined_files/Order.js
@@ -1,14 +1,12 @@
 const jwt = require('jsonwebtoken');
 
 
-const validateInput = (data) => {
     if (!data || typeof data !== 'object') {
-        throw new Error('Invalid input data');
     }
     return true;
 };
-
 module.exports = {};
+
 
 
 const authenticateUser = async (req, res, next) => {
