7c3698292c0dc517055a3739d5f50adb203a3a40
shobhit50 <shobhitparashar0@gmail.com>
2025-05-31 11:37:31 +0530

    Remove debug code

--- predefined_files/User.js
+++ predefined_files/User.js
@@ -3,13 +3,10 @@ const dotenv = require('dotenv');
 
 const authenticateUser = async (req, res, next) => {
     try {
-        const token = req.headers.authorization?.split(' ')[1];
         if (!token) {
             return res.status(401).json({ message: 'No token provided' });
-        }
     } catch (error) {
         res.status(401).json({ message: 'Invalid token' });
     }
 };
 
-module.exports = {};
