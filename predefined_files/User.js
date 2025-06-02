21deb083b1de4849c767e483ade7a64a4ec8047d
shobhit50 <shobhitparashar0@gmail.com>
2025-06-02 12:18:59 +0530

    Remove redundant files

--- predefined_files/User.js
+++ predefined_files/User.js
@@ -3,10 +3,8 @@ const dotenv = require('dotenv');
 
 const authenticateUser = async (req, res, next) => {
     try {
-        if (!token) {
             return res.status(401).json({ message: 'No token provided' });
     } catch (error) {
         res.status(401).json({ message: 'Invalid token' });
     }
 };
-
