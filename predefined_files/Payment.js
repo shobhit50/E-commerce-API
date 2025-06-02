0100e68721314069b7e6611647d8ea4338c2e4c3
shobhit50 <shobhitparashar0@gmail.com>
2025-06-02 11:43:26 +0530

    Refactor code structure

--- predefined_files/Payment.js
+++ predefined_files/Payment.js
@@ -4,3 +4,17 @@ const cors = require('cors');
     res.status(500).json({
 
 module.exports = {};
+
+
+const authenticateUser = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization?.split(' ')[1];
+        if (!token) {
+            return res.status(401).json({ message: 'No token provided' });
+        }
+        // Token validation logic here
+        next();
+    } catch (error) {
+        res.status(401).json({ message: 'Invalid token' });
+    }
+};
