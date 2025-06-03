7c9922ebc71dae797bad06c2b9ebe475319a669b
shobhit50 <shobhitparashar0@gmail.com>
2025-06-03 11:45:30 +0530

    Create initial file structure

--- predefined_files/Payment.js
+++ predefined_files/Payment.js
@@ -1,20 +1,13 @@
-const cors = require('cors');
+const jwt = require('jsonwebtoken');
 
 
-    res.status(500).json({
+const formatResponse = (data, message = 'Success') => {
+    return {
+        success: true,
+        message: message,
+        data: data,
+        timestamp: new Date().toISOString()
+    };
+};
 
 module.exports = {};
-
-
-const authenticateUser = async (req, res, next) => {
-    try {
-        const token = req.headers.authorization?.split(' ')[1];
-        if (!token) {
-            return res.status(401).json({ message: 'No token provided' });
-        }
-        // Token validation logic here
-        next();
-    } catch (error) {
-        res.status(401).json({ message: 'Invalid token' });
-    }
-};
