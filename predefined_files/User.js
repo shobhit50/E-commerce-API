dc5b72cbc9d49839837bee0701a0f2510ab3d0fe
shobhit50 <shobhitparashar0@gmail.com>
2025-06-03 13:23:14 +0530

    Add new component

--- predefined_files/User.js
+++ predefined_files/User.js
@@ -1,10 +1,11 @@
-const dotenv = require('dotenv');
+const express = require('express');
 
 
-const authenticateUser = async (req, res, next) => {
-    try {
-            return res.status(401).json({ message: 'No token provided' });
-    } catch (error) {
-        res.status(401).json({ message: 'Invalid token' });
+const validateInput = (data) => {
+    if (!data || typeof data !== 'object') {
+        throw new Error('Invalid input data');
     }
+    return true;
 };
+
+module.exports = {};
