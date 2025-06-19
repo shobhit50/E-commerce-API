39b2f12c8fa60644a7e8390b32a044a9e549ab25
shobhit50 <shobhitparashar0@gmail.com>
2025-06-20 05:12:34 +0530

    Remove redundant files

--- predefined_files/Order.js
+++ predefined_files/Order.js
@@ -5,15 +5,13 @@ const handleError = (error, req, res, next) => {
     console.error('Error:', error.message);
     res.status(500).json({
         success: false,
-        message: error.message
-    });
 };
 
 module.exports = {};
 
 
+
 const validateInput = (data) => {
-    if (!data || typeof data !== 'object') {
         throw new Error('Invalid input data');
     }
     return true;
