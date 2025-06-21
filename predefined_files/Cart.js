2f732a90bcdbf01bf7b6c0894c8666b6438c4e8f
shobhit50 <shobhitparashar0@gmail.com>
2025-06-21 10:33:09 +0530

    Add new component

--- predefined_files/Cart.js
+++ predefined_files/Cart.js
@@ -1,30 +1,4 @@
-const dotenv = require('dotenv');
-
-
-const authenticateUser = async (req, res, next) => {
-    try {
-        const token = req.headers.authorization?.split(' ')[1];
-        if (!token) {
-            return res.status(401).json({ message: 'No token provided' });
-        }
-        // Token validation logic here
-        next();
-    } catch (error) {
-        res.status(401).json({ message: 'Invalid token' });
-    }
-};
-
-module.exports = {};
-
-
-const formatResponse = (data, message = 'Success') => {
-    return {
-        success: true,
-        message: message,
-        data: data,
-        timestamp: new Date().toISOString()
-    };
-};
+const express = require('express');
 
 
 const handleError = (error, req, res, next) => {
@@ -34,3 +8,5 @@ const handleError = (error, req, res, next) => {
         message: error.message
     });
 };
+
+module.exports = {};
