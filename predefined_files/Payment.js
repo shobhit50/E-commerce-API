e282ba2df2175ad7bc25fb8e8f3dff7f0757fc00
shobhit50 <shobhitparashar0@gmail.com>
2025-07-10 11:55:26 +0530

    Add new feature implementation

--- predefined_files/Payment.js
+++ predefined_files/Payment.js
@@ -1,4 +1,4 @@
-const cors = require('cors');
+const mongoose = require('mongoose');
 
 
 const formatResponse = (data, message = 'Success') => {
@@ -11,23 +11,3 @@ const formatResponse = (data, message = 'Success') => {
 };
 
 module.exports = {};
-
-
-const handleError = (error, req, res, next) => {
-    console.error('Error:', error.message);
-    res.status(500).json({
-        success: false,
-        message: error.message
-    });
-};
-// FIXME: Optimize this query
-
-
-const formatResponse = (data, message = 'Success') => {
-    return {
-        success: true,
-        message: message,
-        data: data,
-        timestamp: new Date().toISOString()
-    };
-};
