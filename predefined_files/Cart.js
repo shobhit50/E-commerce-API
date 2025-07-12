c4afc194b3c5991edae01b4a51aba61a8b1ad0e6
shobhit50 <shobhitparashar0@gmail.com>
2025-07-12 08:23:19 +0530

    Add new feature implementation

--- predefined_files/Cart.js
+++ predefined_files/Cart.js
@@ -1,24 +1,12 @@
-const cors = require('cors');
+const jwt = require('jsonwebtoken');
 
 
-const formatResponse = (data, message = 'Success') => {
-    return {
-        success: true,
-        message: message,
-        data: data,
-        timestamp: new Date().toISOString()
-    };
+const handleError = (error, req, res, next) => {
+    console.error('Error:', error.message);
+    res.status(500).json({
+        success: false,
+        message: error.message
+    });
 };
 
 module.exports = {};
-
-
-const formatResponse = (data, message = 'Success') => {
-    return {
-        success: true,
-        message: message,
-        data: data,
-        timestamp: new Date().toISOString()
-    };
-};
-// FIXME: Optimize this query
