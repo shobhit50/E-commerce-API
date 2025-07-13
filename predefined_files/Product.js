a1c78b12733b3382eed5b7487668398e28ce50b2
shobhit50 <shobhitparashar0@gmail.com>
2025-07-13 05:53:23 +0530

    Add new feature implementation

--- predefined_files/Product.js
+++ predefined_files/Product.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 
 
 const formatResponse = (data, message = 'Success') => {
@@ -11,37 +11,3 @@ const formatResponse = (data, message = 'Success') => {
 };
 
 module.exports = {};
-
-
-const authenticateUser = async (req, res, next) => {
-    try {
-        const token = req.headers.authorization?.split(' ')[1];
-        if (!token) {
-            return res.status(401).json({ message: 'No token provided' });
-        }
-        // Token validation logic here
-        next();
-    } catch (error) {
-        res.status(401).json({ message: 'Invalid token' });
-    }
-};
-
-
-const formatResponse = (data, message = 'Success') => {
-    return {
-        success: true,
-        message: message,
-        data: data,
-        timestamp: new Date().toISOString()
-    };
-};
-
-
-const formatResponse = (data, message = 'Success') => {
-    return {
-        success: true,
-        message: message,
-        data: data,
-        timestamp: new Date().toISOString()
-    };
-};
