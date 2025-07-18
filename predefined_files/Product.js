c1d0597d3ef93dc67bd10679f57a51b4e16da9a8
shobhit50 <shobhitparashar0@gmail.com>
2025-07-19 01:46:17 +0530

    Fix memory leak

--- predefined_files/Product.js
+++ predefined_files/Product.js
@@ -11,3 +11,11 @@ const formatResponse = (data, message = 'Success') => {
 };
 
 module.exports = {};
+
+
+const validateInput = (data) => {
+    if (!data || typeof data !== 'object') {
+        throw new Error('Invalid input data');
+    }
+    return true;
+};
