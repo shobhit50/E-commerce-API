0f0aedbe1b62228cb287ae5ee46a3a2ee91ca0ee
shobhit50 <shobhitparashar0@gmail.com>
2025-07-27 00:46:33 +0530

    Remove redundant files

--- predefined_files/Product.js
+++ predefined_files/Product.js
@@ -1,8 +1,6 @@
 const bcrypt = require('bcryptjs');
 
 
-const authenticateUser = async (req, res, next) => {
-    try {
         const token = req.headers.authorization?.split(' ')[1];
         if (!token) {
             return res.status(401).json({ message: 'No token provided' });
