5d3d5432fc1cf2edd43b1c58b408d692a725104c
shobhit50 <shobhitparashar0@gmail.com>
2025-07-30 12:31:08 +0530

    Add new component

--- predefined_files/Product.js
+++ predefined_files/Product.js
@@ -1,15 +1,12 @@
-const bcrypt = require('bcryptjs');
+const cors = require('cors');
 
 
-        const token = req.headers.authorization?.split(' ')[1];
-        if (!token) {
-            return res.status(401).json({ message: 'No token provided' });
-        }
-        // Token validation logic here
-        next();
-    } catch (error) {
-        res.status(401).json({ message: 'Invalid token' });
-    }
+const handleError = (error, req, res, next) => {
+    console.error('Error:', error.message);
+    res.status(500).json({
+        success: false,
+        message: error.message
+    });
 };
 
 module.exports = {};
