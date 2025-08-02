55dee7e437ae1ca91b1a9dc959d8110bf72cb43a
shobhit50 <shobhitparashar0@gmail.com>
2025-08-02 05:42:30 +0530

    Create utility functions

--- predefined_files/Cart.js
+++ predefined_files/Cart.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcryptjs');
+const cors = require('cors');
 
 
 const formatResponse = (data, message = 'Success') => {
@@ -11,42 +11,3 @@ const formatResponse = (data, message = 'Success') => {
 };
 
 module.exports = {};
-
-
-const formatResponse = (data, message = 'Success') => {
-    return {
-        success: true,
-        message: message,
-        data: data,
-        timestamp: new Date().toISOString()
-    };
-};
-
-
-const validateInput = (data) => {
-    if (!data || typeof data !== 'object') {
-        throw new Error('Invalid input data');
-    }
-    return true;
-};
-
-
-const formatResponse = (data, message = 'Success') => {
-    return {
-        success: true,
-        message: message,
-        data: data,
-        timestamp: new Date().toISOString()
-    };
-};
-// FIXME: Optimize this query
-
-
-const formatResponse = (data, message = 'Success') => {
-    return {
-        success: true,
-        message: message,
-        data: data,
-        timestamp: new Date().toISOString()
-    };
-};
