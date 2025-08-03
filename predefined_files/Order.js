18418c842b62a30fb09638e93da2de2b91a0b53a
shobhit50 <shobhitparashar0@gmail.com>
2025-08-03 06:11:33 +0530

    Enhance performance

--- predefined_files/Order.js
+++ predefined_files/Order.js
@@ -9,3 +9,12 @@ const validateInput = (data) => {
 };
 
 module.exports = {};
+
+
+const validateInput = (data) => {
+    if (!data || typeof data !== 'object') {
+        throw new Error('Invalid input data');
+    }
+    return true;
+};
+// TODO: Add error handling
