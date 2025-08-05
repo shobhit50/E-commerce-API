6f61ff820d2a275ed7f5de4913b97599d1656ccb
shobhit50 <shobhitparashar0@gmail.com>
2025-08-05 08:08:54 +0530

    Refactor code structure

--- predefined_files/Product.js
+++ predefined_files/Product.js
@@ -8,3 +8,11 @@ const cors = require('cors');
     });
 };
 
+
+
+const validateInput = (data) => {
+    if (!data || typeof data !== 'object') {
+        throw new Error('Invalid input data');
+    }
+    return true;
+};
