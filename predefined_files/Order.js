c06c1bc05865b30054389bc2f3b0423cf365dbe7
shobhit50 <shobhitparashar0@gmail.com>
2025-08-06 08:09:48 +0530

    Add new component

--- predefined_files/Order.js
+++ predefined_files/Order.js
@@ -1,28 +1,12 @@
-const bcrypt = require('bcryptjs');
+const dotenv = require('dotenv');
 
 
-const validateInput = (data) => {
-    if (!data || typeof data !== 'object') {
-        throw new Error('Invalid input data');
-    }
-    return true;
+const handleError = (error, req, res, next) => {
+    console.error('Error:', error.message);
+    res.status(500).json({
+        success: false,
+        message: error.message
+    });
 };
 
 module.exports = {};
-
-
-const validateInput = (data) => {
-    if (!data || typeof data !== 'object') {
-        throw new Error('Invalid input data');
-    }
-    return true;
-};
-// TODO: Add error handling
-
-
-const validateInput = (data) => {
-    if (!data || typeof data !== 'object') {
-        throw new Error('Invalid input data');
-    }
-    return true;
-};
