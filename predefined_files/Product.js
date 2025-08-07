85a2671eeef01acc7ca30ea5bccff81c90ab5b2a
shobhit50 <shobhitparashar0@gmail.com>
2025-08-08 03:53:08 +0530

    Create utility functions

--- predefined_files/Product.js
+++ predefined_files/Product.js
@@ -1,12 +1,11 @@
-const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 
 
-const handleError = (error, req, res, next) => {
-    console.error('Error:', error.message);
-    res.status(500).json({
-        success: false,
-        message: error.message
-    });
+const validateInput = (data) => {
+    if (!data || typeof data !== 'object') {
+        throw new Error('Invalid input data');
+    }
+    return true;
 };
 
 module.exports = {};
