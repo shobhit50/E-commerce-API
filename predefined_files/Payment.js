6f97b9b29e60e11780abc0486ea3ee9556342b25
shobhit50 <shobhitparashar0@gmail.com>
2025-08-11 00:30:09 +0530

    Resolve validation issue

--- predefined_files/Payment.js
+++ predefined_files/Payment.js
@@ -24,3 +24,12 @@ const handleError = (error, req, res, next) => {
         message: error.message
     });
 };
+
+
+const validateInput = (data) => {
+    if (!data || typeof data !== 'object') {
+        throw new Error('Invalid input data');
+    }
+    return true;
+};
+// NOTE: This needs refactoring
