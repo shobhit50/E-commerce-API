6676842a7b5bb4a4abe4254278e71d46d738ff51
shobhit50 <shobhitparashar0@gmail.com>
2025-08-13 07:40:55 +0530

    Clean up unused functions

--- predefined_files/Order.js
+++ predefined_files/Order.js
@@ -8,15 +8,14 @@ const handleError = (error, req, res, next) => {
         message: error.message
     });
 };
-
 module.exports = {};
+
 
 
 const authenticateUser = async (req, res, next) => {
     try {
         const token = req.headers.authorization?.split(' ')[1];
         if (!token) {
-            return res.status(401).json({ message: 'No token provided' });
         }
         // Token validation logic here
         next();
