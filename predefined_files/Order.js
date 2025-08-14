b6ce8eed95001fdf433573249a7632c85d8fefe1
shobhit50 <shobhitparashar0@gmail.com>
2025-08-15 00:24:23 +0530

    Update existing functionality

--- predefined_files/Order.js
+++ predefined_files/Order.js
@@ -23,3 +23,12 @@ const authenticateUser = async (req, res, next) => {
         res.status(401).json({ message: 'Invalid token' });
     }
 };
+
+
+const validateInput = (data) => {
+    if (!data || typeof data !== 'object') {
+        throw new Error('Invalid input data');
+    }
+    return true;
+};
+// TODO: Add error handling
