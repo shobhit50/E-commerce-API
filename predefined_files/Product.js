e9271de2e7a9478e4871239eec145f1b7ca25f23
shobhit50 <shobhitparashar0@gmail.com>
2025-08-16 10:35:02 +0530

    Clean up unused functions

--- predefined_files/Product.js
+++ predefined_files/Product.js
@@ -1,11 +1,8 @@
 const jwt = require('jsonwebtoken');
 
 
-const validateInput = (data) => {
     if (!data || typeof data !== 'object') {
         throw new Error('Invalid input data');
-    }
     return true;
 };
 
-module.exports = {};
