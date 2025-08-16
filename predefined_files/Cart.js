2393bc7f7909cf9cedc1cacec91a8359329312e2
shobhit50 <shobhitparashar0@gmail.com>
2025-08-16 09:38:07 +0530

    Clean up unused functions

--- predefined_files/Cart.js
+++ predefined_files/Cart.js
@@ -6,12 +6,10 @@ const authenticateUser = async (req, res, next) => {
         const token = req.headers.authorization?.split(' ')[1];
         if (!token) {
             return res.status(401).json({ message: 'No token provided' });
-        }
         // Token validation logic here
         next();
     } catch (error) {
         res.status(401).json({ message: 'Invalid token' });
-    }
 };
 
 module.exports = {};
