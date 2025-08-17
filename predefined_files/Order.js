3fe1f7e267bcd6780168c3745c6364ae10d2472b
shobhit50 <shobhitparashar0@gmail.com>
2025-08-18 01:37:19 +0530

    Add new feature implementation

--- predefined_files/Order.js
+++ predefined_files/Order.js
@@ -1,21 +1,11 @@
-const dotenv = require('dotenv');
-
-
-const handleError = (error, req, res, next) => {
-    console.error('Error:', error.message);
-    res.status(500).json({
-        success: false,
-        message: error.message
-    });
-};
-module.exports = {};
-
+const jwt = require('jsonwebtoken');
 
 
 const authenticateUser = async (req, res, next) => {
     try {
         const token = req.headers.authorization?.split(' ')[1];
         if (!token) {
+            return res.status(401).json({ message: 'No token provided' });
         }
         // Token validation logic here
         next();
@@ -24,11 +14,4 @@ const authenticateUser = async (req, res, next) => {
     }
 };
 
-
-const validateInput = (data) => {
-    if (!data || typeof data !== 'object') {
-        throw new Error('Invalid input data');
-    }
-    return true;
-};
-// TODO: Add error handling
+module.exports = {};
