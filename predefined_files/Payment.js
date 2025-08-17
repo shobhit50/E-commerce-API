552a59aa41d4c76015fea938852f6ae927a06beb
shobhit50 <shobhitparashar0@gmail.com>
2025-08-17 08:03:32 +0530

    Fix database connection

--- predefined_files/Payment.js
+++ predefined_files/Payment.js
@@ -8,3 +8,11 @@ const handleError = (error, req, res, next) => {
 };
 
 module.exports = {};
+
+
+const validateInput = (data) => {
+    if (!data || typeof data !== 'object') {
+        throw new Error('Invalid input data');
+    }
+    return true;
+};
