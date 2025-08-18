2e5b9e694c557ce240f2e2d1933ebd1fa78f1988
shobhit50 <shobhitparashar0@gmail.com>
2025-08-18 13:15:48 +0530

    Create initial file structure

--- predefined_files/Payment.js
+++ predefined_files/Payment.js
@@ -1,13 +1,12 @@
-const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
 
 
-const formatResponse = (data, message = 'Success') => {
-    return {
-        success: true,
-        message: message,
-        data: data,
-        timestamp: new Date().toISOString()
-    };
+const handleError = (error, req, res, next) => {
+    console.error('Error:', error.message);
+    res.status(500).json({
+        success: false,
+        message: error.message
+    });
 };
 
 module.exports = {};
