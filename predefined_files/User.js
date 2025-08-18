b0324def48c972f7f71b0f9205b96d049d057a34
shobhit50 <shobhitparashar0@gmail.com>
2025-08-19 05:11:36 +0530

    Delete obsolete comments

--- predefined_files/User.js
+++ predefined_files/User.js
@@ -4,8 +4,6 @@ const express = require('express');
 const authenticateUser = async (req, res, next) => {
     try {
         const token = req.headers.authorization?.split(' ')[1];
-        if (!token) {
-            return res.status(401).json({ message: 'No token provided' });
         }
         // Token validation logic here
         next();
