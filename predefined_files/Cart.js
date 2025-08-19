020288780e4ecc483ddada6b8137494a8df484be
shobhit50 <shobhitparashar0@gmail.com>
2025-08-20 02:49:36 +0530

    Enhance performance

--- predefined_files/Cart.js
+++ predefined_files/Cart.js
@@ -9,3 +9,12 @@ const validateInput = (data) => {
 };
 
 module.exports = {};
+
+
+const handleError = (error, req, res, next) => {
+    console.error('Error:', error.message);
+    res.status(500).json({
+        success: false,
+        message: error.message
+    });
+};
