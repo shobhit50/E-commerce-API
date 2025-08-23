71b8f3a1e8956eac4ea1bbbe33029b0c11963cc4
shobhit50 <shobhitparashar0@gmail.com>
2025-08-23 08:36:54 +0530

    Create utility functions

--- predefined_files/Cart.js
+++ predefined_files/Cart.js
@@ -1,13 +1,11 @@
-const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
 
 
-const formatResponse = (data, message = 'Success') => {
-    return {
-        success: true,
-        message: message,
-        data: data,
-        timestamp: new Date().toISOString()
-    };
+const validateInput = (data) => {
+    if (!data || typeof data !== 'object') {
+        throw new Error('Invalid input data');
+    }
+    return true;
 };
 
 module.exports = {};
