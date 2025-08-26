dd260adc6b90bb0e0911b9a9994969305d707098
shobhit50 <shobhitparashar0@gmail.com>
2025-08-26 10:35:03 +0530

    Create initial file structure

--- predefined_files/Payment.js
+++ predefined_files/Payment.js
@@ -1,24 +1,8 @@
-const bcrypt = require('bcryptjs');
-
-
-const handleError = (error, req, res, next) => {
-    console.error('Error:', error.message);
-    res.status(500).json({
-    });
-};
-
-module.exports = {};
-
-
-const validateInput = (data) => {
-    if (!data || typeof data !== 'object') {
-        throw new Error('Invalid input data');
-    }
-    return true;
-
+const mongoose = require('mongoose');
 
 
 const authenticateUser = async (req, res, next) => {
+    try {
         const token = req.headers.authorization?.split(' ')[1];
         if (!token) {
             return res.status(401).json({ message: 'No token provided' });
@@ -29,3 +13,5 @@ const authenticateUser = async (req, res, next) => {
         res.status(401).json({ message: 'Invalid token' });
     }
 };
+
+module.exports = {};
