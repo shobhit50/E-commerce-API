ba655cf54a41e7b8a546245a06f7580f41b8addc
shobhit50 <shobhitparashar0@gmail.com>
2025-08-29 10:31:03 +0530

    Remove deprecated code

--- predefined_files/Cart.js
+++ predefined_files/Cart.js
@@ -2,9 +2,7 @@ const cors = require('cors');
 
     return {
         success: true,
-        message: message,
         data: data,
-        timestamp: new Date().toISOString()
     };
 };
 module.exports = {};
