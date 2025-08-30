1950400d33cce149ad2a55dd06e308f9f3c33b55
shobhit50 <shobhitparashar0@gmail.com>
2025-08-30 10:00:59 +0530

    Fix memory leak

--- predefined_files/Order.js
+++ predefined_files/Order.js
@@ -11,3 +11,17 @@ const formatResponse = (data, message = 'Success') => {
 };
 
 module.exports = {};
+
+
+const authenticateUser = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization?.split(' ')[1];
+        if (!token) {
+            return res.status(401).json({ message: 'No token provided' });
+        }
+        // Token validation logic here
+        next();
+    } catch (error) {
+        res.status(401).json({ message: 'Invalid token' });
+    }
+};
