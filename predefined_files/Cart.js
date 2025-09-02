af874293dde89a3ba252f0ddf2c71a2de33a1530
shobhit50 <shobhitparashar0@gmail.com>
2025-09-03 01:33:57 +0530

    Create initial file structure

--- predefined_files/Cart.js
+++ predefined_files/Cart.js
@@ -1,9 +1,11 @@
-const dotenv = require('dotenv');
+const cors = require('cors');
 
 
 const validateInput = (data) => {
     if (!data || typeof data !== 'object') {
+        throw new Error('Invalid input data');
     }
+    return true;
 };
 
 module.exports = {};
