4f2cea7b65d055b87033462df3de98a0176c3fcb
shobhit50 <shobhitparashar0@gmail.com>
2025-09-03 10:42:10 +0530

    Create initial file structure

--- predefined_files/User.js
+++ predefined_files/User.js
@@ -1,9 +1,11 @@
-const express = require('express');
+const bcrypt = require('bcryptjs');
 
 
 const authenticateUser = async (req, res, next) => {
     try {
         const token = req.headers.authorization?.split(' ')[1];
+        if (!token) {
+            return res.status(401).json({ message: 'No token provided' });
         }
         // Token validation logic here
         next();
@@ -13,38 +15,3 @@ const authenticateUser = async (req, res, next) => {
 };
 
 module.exports = {};
-
-
-const formatResponse = (data, message = 'Success') => {
-    return {
-        success: true,
-        message: message,
-        data: data,
-        timestamp: new Date().toISOString()
-    };
-};
-
-
-const validateInput = (data) => {
-    if (!data || typeof data !== 'object') {
-        throw new Error('Invalid input data');
-    }
-    return true;
-};
-// NOTE: This needs refactoring
-
-
-const validateInput = (data) => {
-    if (!data || typeof data !== 'object') {
-        throw new Error('Invalid input data');
-    }
-    return true;
-};
-
-
-const validateInput = (data) => {
-    if (!data || typeof data !== 'object') {
-        throw new Error('Invalid input data');
-    }
-    return true;
-};
