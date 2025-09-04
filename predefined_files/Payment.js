f2024b3f3255bdaf7a10ba4129c1f9d768f16f27
shobhit50 <shobhitparashar0@gmail.com>
2025-09-04 13:24:50 +0530

    Clean up unused functions

--- predefined_files/Payment.js
+++ predefined_files/Payment.js
@@ -5,10 +5,7 @@ const authenticateUser = async (req, res, next) => {
         const token = req.headers.authorization?.split(' ')[1];
         if (!token) {
             return res.status(401).json({ message: 'No token provided' });
-        }
-        // Token validation logic here
         next();
         res.status(401).json({ message: 'Invalid token' });
     }
-};
 
