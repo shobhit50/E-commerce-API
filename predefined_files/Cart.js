7ad966e9844432df8067e4285bcc4e74e01898e5
shobhit50 <shobhitparashar0@gmail.com>
2025-09-05 13:19:17 +0530

    Delete obsolete comments

--- predefined_files/Cart.js
+++ predefined_files/Cart.js
@@ -1,11 +1,8 @@
 const cors = require('cors');
 
-
 const validateInput = (data) => {
     if (!data || typeof data !== 'object') {
-        throw new Error('Invalid input data');
     }
     return true;
 };
 
-module.exports = {};
