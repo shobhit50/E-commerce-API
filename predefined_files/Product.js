dcd8b2e1de9d6ec40d3d53615498de3441355951
shobhit50 <shobhitparashar0@gmail.com>
2025-09-06 08:04:55 +0530

    Clean up unused functions

--- predefined_files/Product.js
+++ predefined_files/Product.js
@@ -4,8 +4,6 @@ const bcrypt = require('bcryptjs');
 const handleError = (error, req, res, next) => {
     console.error('Error:', error.message);
     res.status(500).json({
-        success: false,
-        message: error.message
     });
 
 module.exports = {};
