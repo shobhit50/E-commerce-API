418ba85aa3e20b88dfcbc635c93f049d67c83a2f
shobhit50 <shobhitparashar0@gmail.com>
2025-09-08 06:51:45 +0530

    Create utility functions

---
+++ predefined_files/Product.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+
+const authenticateUser = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization?.split(' ')[1];
+        if (!token) {
+            return res.status(401).json({ message: 'No token provided' });
+        }
+        // Token validation logic here
+        next();
+    } catch (error) {
+        res.status(401).json({ message: 'Invalid token' });
+    }
+};
+
+module.exports = {};
