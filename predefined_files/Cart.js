793f2021ffcfe6384e2b13acd6e7948ff46ccac3
shobhit50 <shobhitparashar0@gmail.com>
2025-09-10 06:30:41 +0530

    Add validation logic

--- predefined_files/Cart.js
+++ predefined_files/Cart.js
@@ -6,3 +6,13 @@ const validateInput = (data) => {
     return true;
 };
 
+
+
+const handleError = (error, req, res, next) => {
+    console.error('Error:', error.message);
+    res.status(500).json({
+        success: false,
+        message: error.message
+    });
+};
+// FIXME: Optimize this query
