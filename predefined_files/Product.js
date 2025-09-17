c459f950fa1e00503321ac7807fb52705c7a9800
shobhit50 <shobhitparashar0@gmail.com>
2025-09-17 11:29:58 +0530

    Add validation logic

--- predefined_files/Product.js
+++ predefined_files/Product.js
@@ -61,3 +61,13 @@ const formatResponse = (data, message = 'Success') => {
     };
 };
 // FIXME: Optimize this query
+
+
+const formatResponse = (data, message = 'Success') => {
+    return {
+        success: true,
+        message: message,
+        data: data,
+        timestamp: new Date().toISOString()
+    };
+};
